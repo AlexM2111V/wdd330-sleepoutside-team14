219c99e6210eeaa209236e30314eaa9be2a4a70a
SmithFerreira-07 <GitFerreira@proton.me>
2025-09-16 17:58:40 -0300

    Added JSON/Alerts.mjs + team.txt added my name

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
+import Alert from "./Alert.mjs";
 
 const dataSource = new ProductData("tents");
 
@@ -7,4 +8,7 @@ const element = document.querySelector(".product-list");
 
 const productList = new ProductList("Tents", dataSource, element);
 
+const alertManager = new Alert();
+
+alertManager.displayAlerts();
 productList.init();
