199e2669027b79e4e900dc47512b41254f8b5b5f
AlexM2111V <jesusmendozav2111@gmail.com>
2025-09-25 22:28:54 -0500

    W04 Team Activity: Checkout

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,6 +1,8 @@
 import { getLocalStorage } from "./utils.mjs";
 import { loadHeaderFooter } from "./utils.mjs";
 
+loadHeaderFooter();
+
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
@@ -11,16 +13,13 @@ function cartItemTemplate(item) {
   const newItem = `<li class="cart-card divider">
   <span class="cart-card__remove" data-id="${item.Id}">X</span>
   <a href="#" class="cart-card__image">
-    <img
-      src="${item.Image}"
-      alt="${item.Name}"
-    />
+    <img src="${item.Images.PrimaryMedium}" alt="${item.Name}"/>
   </a>
   <a href="#">
     <h2 class="card__name">${item.Name}</h2>
   </a>
   <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-  <p class="cart-card__quantity">qty: 1</p>
+  <p class="cart-card__quantity">Qty: 1</p>
   <p class="cart-card__price">$${item.FinalPrice}</p>
 </li>`;
 
@@ -30,7 +29,7 @@ function cartItemTemplate(item) {
 function removeItemFromCart(e) {
   const cartItems = getLocalStorage("so-cart");
   const itemId = e.target.dataset.id;
-  
+
   //! fix for removing multiple items of the same ID DO NOT REMOVE THIS
   const itemIndex = cartItems.findIndex((item) => item.Id == itemId);
   if (itemIndex !== -1) {
@@ -41,6 +40,7 @@ function removeItemFromCart(e) {
 }
 
 renderCartContents();
-loadHeaderFooter();
 
-document.querySelector(".product-list").addEventListener("click", removeItemFromCart);
+document
+  .querySelector(".product-list")
+  .addEventListener("click", removeItemFromCart);
